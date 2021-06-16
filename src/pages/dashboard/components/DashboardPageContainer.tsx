2fc8748e2edbe474d1e426d103a94c886d17921b
Liam McMains <lmcmains@codeauthority.com>
2021-06-16 17:19:42 +0000

    Merged PR 3390: Initial project setup from snowpack starter project
    • Contains an initial project setup with routing, a few basic pages, and tailwind/styled components for styling.
    • Want to get at least one push of the actual application into the develop branch so that we can test out Azure Pipelines.
    • Obviously not ready for client to view yet.

---
+++ src/pages/dashboard/components/DashboardPageContainer.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+  color: white;
+`;
---
+++ src/pages/dashboard/components/DashboardTrainingList.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import DashboardTrainingItemRow, {
+  TRAINING_BAR_COLOR,
+} from './DashboardTrainingItem';
+import Column from '../../../components/column';
+import styled from 'styled-components';
+
+export default function DashboardTrainingList() {
+  return (
+    <DashboardListColumn>
+      <AlphabeticTierRow />
+      <AmalgamateTierRow />
+      <LexicalTierRow />
+      <ChordalTierRow />
+
+      <VerticalBlueBar />
+    </DashboardListColumn>
+  );
+}
+
+const AlphabeticTierRow = () => (
+  <DashboardTrainingItemRow
+    tutorialButtonTitle="Orientation"
+    exerciseButtonTitle="Alphabet Training"
+    contentText="0 wpm"
+    tierTitle="Alphabetic"
+    shouldDisplayHeader
+  />
+);
+
+const AmalgamateTierRow = () => (
+  <DashboardTrainingItemRow
+    tutorialButtonTitle="Trigram Tutorial"
+    exerciseButtonTitle="Trigram Training"
+    contentText="Average Writing Speed = 13 wpm"
+    tierTitle="Amalgamate"
+  />
+);
+
+const LexicalTierRow = () => (
+  <DashboardTrainingItemRow
+    tutorialButtonTitle="Lexical Tutorial"
+    exerciseButtonTitle="Lexical Training"
+    contentText="Average Speed of 'Hunt & Peck' Typist = 27 wpm"
+    tierTitle="Lexical"
+  />
+);
+
+const ChordalTierRow = () => (
+  <DashboardTrainingItemRow
+    tutorialButtonTitle="Chording Tutorial"
+    exerciseButtonTitle="Chord Training"
+    contentText="Average Speed of Keyboard User = 40 wpm"
+    tierTitle="Chordal"
+  />
+);
+
+const DashboardListColumn = styled(Column)`
+  width: 100%;
+  position: relative;
+`;
+
+const VerticalBlueBar = styled.div`
+  position: absolute;
+  right: -18px;
+  top: 0;
+  height: 100%;
+  border-right: 16px solid ${TRAINING_BAR_COLOR};
+  z-index: 0;
+`;
