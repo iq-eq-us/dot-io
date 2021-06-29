7afd5e4ab2bc5e68dbd9ce7ee7b1ce9d4a951fcc
Liam McMains <lmcmains@codeauthority.com>
2021-06-29 14:31:01 +0000

    Merged PR 3402: Fix unreported import error causing crash
    Fix unreported import error causing crash

--- src/hooks/useWordsPerMinute.ts
+++ src/hooks/useWordsPerMinute.ts
@@ -1,18 +1,21 @@
-import { getCumulativeValueByPropertyName } from "../pages/training/components/statisticColumn";
-import { useStoreState } from "../store/store"
+import { getCumulativeValueByPropertyName } from '../helpers/aggregation';
+import { useStoreState } from '../store/store';
 
 export const useWordsPerMinute = (): number => {
-  const timeAtTrainingStart = useStoreState((store) => store.timeAtTrainingStart);
+  const timeAtTrainingStart = useStoreState(
+    (store) => store.timeAtTrainingStart,
+  );
   const timeAtTrainingStartInSeconds = timeAtTrainingStart * 0.001;
   const timeNowInSeconds = performance.now() * 0.001;
-  const timeDifferenceInSeconds = timeNowInSeconds - timeAtTrainingStartInSeconds;
+  const timeDifferenceInSeconds =
+    timeNowInSeconds - timeAtTrainingStartInSeconds;
   const timeDifferenceInMinutes = timeDifferenceInSeconds / 60;
   const trainingStatistics = useStoreState((store) => store.trainingStatistics);
 
   const sumOfTrainingOccurrences = getCumulativeValueByPropertyName(
     trainingStatistics.statistics,
     'numberOfOccurrences',
-  )
+  );
 
   return parseFloat(sumOfTrainingOccurrences) / timeDifferenceInMinutes;
-}
+};
