6252eb627f4c33dd7e509637ede0f3ea1a4e8ca6
PrinceOsa <chidiosakwe@outlook.com>
2021-11-07 12:39:57 -0500

    New Chord Manager

--- src/pages/dashboard/components/CardDataRowDisplay.tsx
+++ src/pages/dashboard/components/CardDataRowDisplay.tsx
@@ -40,7 +40,7 @@ export function CardDataRowDisplay(): ReactElement {
         Clear Progress
       </RefreshButton>
       <div className="feather feather-trash-2 p-0.5 text-gray-600">
-      <a href= "https://lemon-flower-01b05aa10.azurestaticapps.net/manager.html"  className="sc-bYwzuL text-white rounded p-2 mb-4 inline-block ml-2 bg-[#333] hover:bg-[#3b3b3b] active:bg-[#222]">Chord Manager</a>
+      <button onClick={() => {history.push(ROUTER_PATHS.manager);} }className="sc-bYwzuL text-white rounded p-2 mb-4 inline-block ml-2 bg-[#333] hover:bg-[#3b3b3b] active:bg-[#222]">Chord Manager</button>
       </div>
     </CardDataRow>
   );
