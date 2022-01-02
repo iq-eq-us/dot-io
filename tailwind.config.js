85ff05237c49eaa8dd64f6d5e4847157a64fbd0f
PrinceOsa <chidiosakwe@outlook.com>
2022-01-01 21:24:14 -0500

    piano

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   mode: 'jit',
-  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   // specify other options here
 };
