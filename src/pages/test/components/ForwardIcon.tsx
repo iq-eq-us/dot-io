4cef9952ff769a22bb6f48de2458a343d088dfbb
Chidi Osakwe <chidiosakwe@charachorder.com>
2022-08-11 22:24:22 -0500

    update site v1

--- src/pages/test/components/ForwardIcon.tsx
+++ src/pages/test/components/ForwardIcon.tsx
@@ -8,9 +8,9 @@ function ForwardIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
     viewBox="0 0 24 24" 
     fill="none" 
     stroke="white" 
-    stroke-width="2" 
-    stroke-linecap="round" 
-    stroke-linejoin="round" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
     className="feather feather-fast-forward">
     <polygon points="13 19 22 12 13 5 13 19"/>
     <polygon points="2 19 11 12 2 5 2 19"/>
