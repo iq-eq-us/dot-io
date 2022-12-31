5493867e824f4b29b9f681f3d77fe0807728bd88
Chidi Osakwe <chidiosakwe@charachorder.com>
2022-12-31 11:32:08 -0600

    UpgradeCML

--- src/pages/manager/components/upgradeFunction.tsx
+++ src/pages/manager/components/upgradeFunction.tsx
@@ -1,14 +1,11 @@
 import React, { ReactElement } from 'react';
 import {
-    MainControls,
      sendCommandString, 
-     selectBase,
-     readGetOneChordmap
     } from '../controls/mainControls'
-    import {resetDataTable} from '../../manager/components/resetDataTable'
+
 
 export async function upgrade(){
-    await sendCommandString("RST UPGRADE")
+    await sendCommandString("RST UPGRADECML")
     
   }
   export function UpgradeFunction(): ReactElement {
@@ -20,7 +17,7 @@ export async function upgrade(){
       className="text-white rounded p-2 mb-4 inline-block ml-2 bg-[#333] hover:bg-[#3b3b3b] active:bg-[#222] position-absolute"
       color="pink"
       onClick={() => upgrade()}
-      >Upgrade Chords </button>
+      >Upgrade Library </button>
       </React.Fragment>
     );
   }
