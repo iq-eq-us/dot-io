9d5314fdab48b6cb877cd0525ef970d017c774ce
Chidi Osakwe <chidiosakwe@charachorder.com>
2023-08-07 23:30:19 -0500

    Added webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
-module.exports = {
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-          'process.env.MY_ENV': JSON.stringify(process.env.MY_ENV),
-        })
-    ],
-    }
+export const plugins = [
+    new webpack.DefinePlugin({
+        process: { env: {} }
+    })
+];
