8346d3b732142db32632ca376b0272c84ce4c6de
Chidi Osakwe <chidiosakwe@charachorder.com>
2023-08-08 00:35:32 -0500

    Update the process.env variables to import.meta.env

--- src/serviceWorkerRegistration.tsx
+++ src/serviceWorkerRegistration.tsx
@@ -40,7 +40,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${import.meta.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${import.meta.env.PUBLIC_URL}/#/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
